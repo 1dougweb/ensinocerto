17a9e16744ddc4a76c6eac827447c06ffcc72b8d
Douglas <douglaseps@gmail.com>
2025-08-25 20:36:25 -0300

    fix landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -376,8 +376,19 @@
     }
     </script>
     @endverbatim
-    </head>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-NPXJKW38');</script>
+    <!-- End Google Tag Manager -->
+</head>
 <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NPXJKW38"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <!-- Banner de Consentimento de Cookies LGPD -->
     <div id="cookie-consent">
         <div class="cookie-content">
